d91e0c25800885ebd2785a88afb6228ee49cfa1a
MD AWAIZ <awaizkhanmd@gmail.com>
2023-02-02 02:19:29 +0530

    90- changes to edit the basic info

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,18 +3,17 @@ import axios from 'axios';
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    params: {
-        maxResults: 50,
-    },
-    headers: {
-        'X-RapidAPI-Key': 'cf47d563b1mshf6a90f30828f881p12e5f0jsn1d960fd90893',
-        // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    },
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'cf47d563b1mshf6a90f30828f881p12e5f0jsn1d960fd90893',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
 };
 
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-    return data;
+  return data;
 };
